983ac36dc5d43eb2354ff31943de27170bf3dc0f
Maik Goebel <mgoebe10@smail.uni-koeln.de>
2022-01-21 16:31:31 +0100

    HW12 final

---
+++ demo_Spring/src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,101 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.BookService;
+import com.example.demo.datamodel.Book;
+
+@RequestMapping("/bookStore")
+@RestController
+public class BookController {
+
+	@Autowired
+	BookService bookSer;
+	
+	@GetMapping
+	public ResponseEntity<List<Book>> getAllbooks() {
+		
+		List<Book> books = new ArrayList<Book> ();
+		books = bookSer.findBooks();
+		
+		return new ResponseEntity<>(books, HttpStatus.OK);	
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Book> getBookById(@PathVariable int id) {
+		
+		return new ResponseEntity<>(bookSer.fetchBook(id).get(), HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Book> addBook(@RequestBody Book book) {
+		
+		bookSer.addBook(book);
+		
+		return new ResponseEntity<>(book, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Book> removeBookById(@PathVariable int id) {
+		
+		Book book = bookSer.fetchBook(id).get();
+		
+		if (bookSer.deleteBook(id)) {
+			return new ResponseEntity<>(book, HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<>(book, HttpStatus.BAD_REQUEST);
+		}	
+	}
+	
+	@GetMapping("/oldest")
+	public ResponseEntity<Book> getOldestBook() {
+		
+		List<Book> books = new ArrayList<Book> ();
+		books = bookSer.findBooks();
+		
+		int oldest = books.get(0).getYear();
+		int oldestID = 0;
+		
+		for(int i = 1; i < books.size(); i++) {
+			if (oldest > books.get(i).getYear()) {
+				oldest = books.get(i).getYear();
+				oldestID = i;
+			}
+		}
+		
+		return new ResponseEntity<>(bookSer.fetchBook(oldestID+1).get(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/latest")
+	public ResponseEntity<Book> getLatestBook() {
+		
+		List<Book> books = new ArrayList<Book> ();
+		books = bookSer.findBooks();
+		
+		int latest = books.get(0).getYear();
+		int latestID = 0;
+		
+		for(int i = 1; i < books.size(); i++) {
+			if (latest < books.get(i).getYear()) {
+				latest = books.get(i).getYear();
+				latestID = i;
+			}
+		}
+		
+		return new ResponseEntity<>(bookSer.fetchBook(latestID+1).get(), HttpStatus.OK);
+	}
+	
+}
---
+++ demo_Spring/src/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,48 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.repository.BookRepo;
+import com.example.demo.datamodel.Book;
+
+@Service
+public class BookService {
+
+	@Autowired
+	private BookRepo bookRepo;
+	
+	
+	public List<Book> findBooks() {
+		
+		return bookRepo.findAll();		
+	}
+	
+	public Optional<Book> fetchBook(int id) {
+		
+		return bookRepo.findById(id);
+	}
+	
+	public Book addBook(Book book) {
+		
+		return bookRepo.save(book);
+	}
+	
+	public boolean deleteBook(int id) {
+		
+		boolean status;
+		try {
+			bookRepo.deleteById(id);
+			status = true;
+		} catch (Exception e){
+			status = false;
+		}
+		
+		return status;
+	}
+	
+	
+}
